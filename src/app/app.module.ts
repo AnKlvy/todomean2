9ddf0a27e1238048516412908ca5934c60170e36
AnKlvy <andreyklimov.ver2@gmail.com>
2024-02-28 15:15:15 +0600

    added todo component, routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,35 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TodoComponent } from './components/todo/todo.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    // provideAnimationsAsync('noop')
   ],
   bootstrap: [AppComponent]
 })
